a27d1a2b784a4f322ea6e206ab88d06d8f29e5ec
Kasper <kasperkh.kh@gmail.com>
2021-10-09 21:44:58 +0200

    Add left pong player

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
   import { onMount } from 'svelte'
 
+  let points = 0
+
+  let leftY = 0
+  let leftHeight = 40
+
   let x = 0
   let y = 0
   let boxX = 20
@@ -9,14 +14,11 @@
     x = window.innerWidth / 2
     y = window.innerHeight / 2
   })
-  function setPos(newX: number, newY: number) {
-    x = newX
-    y = newY
-    limit()
-  }
   $: maxX = 1000 - boxX / 2
   $: minX = 0 + boxX / 2
   $: maxY = 500 - boxY / 2
+  let tooHigh = 0
+  let tooLow = 0
   function limit() {
     if (x > maxX) x = 1000 - boxX / 2
     if (x < minX) x = 0 + boxX / 2
@@ -24,10 +26,11 @@
     if (y < 0 + boxY / 2) y = 0 + boxY / 2
   }
   function keypress(e: KeyboardEvent) {
-    if (e.key === 's') setPos(x, y + 10)
-    if (e.key === 'w') setPos(x, y - 10)
-    if (e.key === 'd') setPos(x + 10, y)
-    if (e.key === 'a') setPos(x - 10, y)
+    if (e.key === 's') leftY += 10
+    if (e.key === 'w') leftY -= 10
+    if (e.key === 'd') x += 10
+    if (e.key === 'a') x -= 10
+    limit()
   }
   let movePerMillisecond = 0.5
   let last = Date.now()
@@ -47,6 +50,13 @@
         if (x < minX) {
           movingRight = true
           x = minX + (minX - x)
+          if (y < leftY) {
+            tooHigh++
+          } else if (y > leftY + leftHeight) {
+            tooLow++
+          } else {
+            points++
+          }
         }
       }
       window.requestAnimationFrame(step)
@@ -66,8 +76,17 @@
 <svelte:window on:keypress={keypress} />
 
 <input type="range" min="1" max="400" bind:value={sliderValue} />
-<div class="game">
-  <div class="box" style="left: {x}px; top: {y}px; width: {boxX}px; height: {boxY}px;" />
+Points: {points}
+tooHigh: {tooHigh}
+tooLow: {tooLow}
+<div class="outer">
+  <div class="game">
+    <div
+      class="left"
+      style="left: {4 / 2}px; top: {leftY +
+        leftHeight / 2}px; width: 4px; height: {leftHeight}px;" />
+    <div class="box" style="left: {x}px; top: {y}px; width: {boxX}px; height: {boxY}px;" />
+  </div>
 </div>
 
 <style lang="sass">
@@ -81,15 +100,21 @@
   :global(body)
     display: flex
     justify-content: center
-  .game
+  .outer
     border: 1px solid #f2f2f2
+    background-color: #181616
+    overflow: hidden
+  .game
+    margin: 10px
     width: 1000px
     height: 500px
-    overflow: hidden
     position: relative
-    background-color: #181616
   .box
     position: absolute
     transform: translate(-50%, -50%)
     background-color: #1b0896
+  .left
+    position: absolute
+    transform: translate(-50%, -50%)
+    background-color: #ff0000
 </style>
