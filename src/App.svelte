0d64b2fce73129b2e3532bfcb785804bb6b0ecdc
Kasper <kasperkh.kh@gmail.com>
2022-04-25 19:27:59 +0200

    Move global styles to app.css

--- src/App.svelte
+++ src/App.svelte
@@ -10,14 +10,3 @@
 {:else}
   404
 {/if}
-
-<style lang="sass">
-  :global(body)
-    margin: 0px
-    font-family: Arial, Helvetica, sans-serif
-    font-size: 18px
-    background-color: #000000
-    color: #f2f2f2
-    text-align: center
-    min-height: 100%
-</style>
